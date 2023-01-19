7e26c669a4aa0f129ad47cb22a313a0e0dc51315
psalmistcom <dadewale9@gmail.com>
2023-01-19 21:30:21 +0100

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+    <article>
+        <h3>My First Post</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
+    </article>
+    <article>
+        <h3>My First Post</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
+    </article>
+    <article>
+        <h3>My First Post</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
+    </article>
+</body>
+</html>
