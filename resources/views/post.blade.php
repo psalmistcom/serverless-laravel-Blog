7b45f1108f193b5aed61c8a13eb88a3075dd7702
psalmistcom <dadewale9@gmail.com>
2023-01-19 21:44:34 +0100

    some html

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+    <article>
+        <h3><a href="/post">My First Post</a></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
+    </article>
+    <a href="/">Go back</a>
+</body>
+</html>
