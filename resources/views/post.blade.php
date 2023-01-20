77c755335200b9b23dd85f8e99377f8c48df5f00
psalmistcom <dadewale9@gmail.com>
2023-01-20 10:14:08 +0100

    some html resources

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,9 +9,7 @@
 </head>
 <body>
     <article>
-        <h3><a href="/post">My First Post</a></h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, sapiente distinctio enim dolorum dolor soluta iusto. Veniam molestiae deleniti repudiandae explicabo necessitatibus iste eligendi vitae neque. Rerum nisi quis quia?</p>
+       <?= $post; ?>
     </article>
     <a href="/">Go back</a>
 </body>
